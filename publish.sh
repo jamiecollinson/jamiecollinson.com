cd204a16d84ee95e15c82fea925b1f1b29833b70
Jamie Collinson <jamiecollinson@gmail.com>
2017-04-23 15:38:53 +0100

    Update publish script

--- publish.sh
+++ publish.sh
@@ -6,14 +6,12 @@ then
     exit 1;
 fi
 
-echo "Deleting old publication"
+echo "Deleting old public folder"
 rm -rf public
 mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git clone .git --branch gh-pages public
 
 echo "Removing existing files"
 rm -rf public/*
@@ -22,4 +20,4 @@ echo "Generating site"
 hugo
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && git push origin gh-pages
